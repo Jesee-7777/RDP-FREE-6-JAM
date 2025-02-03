bc457e318014920e6d84acd05788865b359c330c
Jesee-7777 <wendiabdullah8@gmail.com>
2025-02-03 07:21:00 +0700

    Delete push.bat

--- push.bat
+++
@@ -1,9 +0,0 @@
-git init
-git remote add origin https://github.com/harapito15/RDP-FREE-6-JAM.git
-git branch -m main
-git status
-git add .
-git status
-git commit -m %1
-git switch main
-git push origin main
